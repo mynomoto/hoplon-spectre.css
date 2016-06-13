6526ba6e77b03e8bb15c74205055330196870dfb
mynomoto <mynomoto@gmail.com>
2016-06-13 20:08:47 -0300

    Add button-primary, wip extracting sections

--- src/hoplon/spectre_css.cljs
+++ src/hoplon/spectre_css.cljs
@@ -1,9 +1,19 @@
 (ns hoplon.spectre-css
-  (:require [hoplon.core :as h]))
+  (:require [clojure.string :as str]
+            [hoplon.core :as h]))
 
 (h/defelem header
   [attrs children]
   (h/header
    (merge-with merge attrs
                {:class "text-center"})
+   children))
+
+(h/defelem button-primary
+  [attrs children]
+  (h/a
+   (merge-with #(str/join " " %1 %2)
+               attrs
+               {:shape "rect",
+                :class "btn btn-primary"})
    children))
