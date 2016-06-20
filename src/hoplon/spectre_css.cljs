0501fa1962cc7c3ec63d1410304035169d32f2ab
mynomoto <mynomoto@gmail.com>
2016-06-20 01:28:22 -0300

    Working on ui elements

--- src/hoplon/spectre_css.cljs
+++ src/hoplon/spectre_css.cljs
@@ -16,3 +16,51 @@
                {:shape "rect"
                 :class "btn btn-primary"})
    children))
+
+(h/defelem button-group
+  [attrs children]
+  (h/div
+   (merge-with #(str %1 " " %2)
+               attrs
+               {:class "btn-group"})
+   children))
+
+(h/defelem button-group-block
+  [attrs children]
+  (h/div
+   (merge-with #(str %1 " " %2)
+               attrs
+               {:class "btn-group btn-group-block"})
+   children))
+
+(h/defelem highlight
+  [attrs children]
+  (h/span
+   (merge-with #(str %1 " " %2)
+               attrs
+               {:class "highlight"})
+   children))
+
+(h/defelem ul
+  [attrs children]
+  (h/ul
+   (merge-with #(str %1 " " %2)
+               attrs
+               {:class "list"})
+   children))
+
+(h/defelem ol
+  [attrs children]
+  (h/ol
+   (merge-with #(str %1 " " %2)
+               attrs
+               {:class "list"})
+   children))
+
+(h/defelem dl
+  [attrs children]
+  (h/dl
+   (merge-with #(str %1 " " %2)
+               attrs
+               {:class "list"})
+   children))
