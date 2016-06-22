9bf914cb7402324d2dd5207ad8f53c02c3c1ef19
mynomoto <mynomoto@gmail.com>
2016-06-22 01:02:19 -0300

    Refactor elements, add table element, remove default attributes

--- src/hoplon/spectre_css.cljs
+++ src/hoplon/spectre_css.cljs
@@ -1,66 +1,69 @@
 (ns hoplon.spectre-css
-  (:require [hoplon.core :as h]))
+  (:require [hoplon.core :as h]
+            [javelin.core :as j]))
 
 (h/defelem header
   [attrs children]
-  (h/header
-   (merge-with merge attrs
-               {:class "text-center"})
+  ((h/header {:class "text-center"})
+   attrs
    children))
 
 (h/defelem button-primary
   [attrs children]
-  (h/a
-   (merge-with #(str %1 " " %2)
-               attrs
-               {:shape "rect"
-                :class "btn btn-primary"})
+  ((h/a {:shape "rect"
+         :class "btn btn-primary"})
+   attrs
    children))
 
 (h/defelem button-group
   [attrs children]
-  (h/div
-   (merge-with #(str %1 " " %2)
-               attrs
-               {:class "btn-group"})
+  ((h/div {:class "btn-group"})
+   attrs
    children))
 
 (h/defelem button-group-block
   [attrs children]
-  (h/div
-   (merge-with #(str %1 " " %2)
-               attrs
-               {:class "btn-group btn-group-block"})
+  ((h/div {:class "btn-group btn-group-block"})
+   attrs
    children))
 
 (h/defelem highlight
   [attrs children]
-  (h/span
-   (merge-with #(str %1 " " %2)
-               attrs
-               {:class "highlight"})
+  ((h/span {:class "highlight"})
+   attrs
    children))
 
 (h/defelem ul
   [attrs children]
-  (h/ul
-   (merge-with #(str %1 " " %2)
-               attrs
-               {:class "list"})
+  ((h/ul {:class "list"})
+   attrs
    children))
 
 (h/defelem ol
   [attrs children]
-  (h/ol
-   (merge-with #(str %1 " " %2)
-               attrs
-               {:class "list"})
+  ((h/ol {:class "list"})
+   attrs
    children))
 
 (h/defelem dl
   [attrs children]
-  (h/dl
-   (merge-with #(str %1 " " %2)
-               attrs
-               {:class "list"})
+  ((h/dl {:class "list"})
+   attrs
    children))
+
+(h/defelem table
+  [{:keys [options] :as attrs} children]
+  (let [table-options {:striped "table-striped"
+                       :hover "table-hover"}
+        class (if (j/cell? options)
+                (j/cell=
+                 (let [options-set (set options)]
+                   {"table-striped" (:striped options-set)
+                    "table-hover" (:hover options-set)
+                    "table" true}))
+                {"table-striped" (:striped options)
+                 "table-hover" (:hover options)
+                 "table" true})]
+    ((h/table :class class)
+     (dissoc attrs :options)
+     children)))
